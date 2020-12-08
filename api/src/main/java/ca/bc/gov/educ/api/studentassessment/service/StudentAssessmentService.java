65832243292a352de735484909e81755636ffffb
s.karekkattumanasree <s.karekkattumanasree@CA-L9Z2KNN2.groupinfra.com>
2020-12-08 09:16:46 -0800

    KeyCloak Changes

--- api/src/main/java/ca/bc/gov/educ/api/studentassessment/service/StudentAssessmentService.java
+++ api/src/main/java/ca/bc/gov/educ/api/studentassessment/service/StudentAssessmentService.java
@@ -8,6 +8,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,6 +19,7 @@ import ca.bc.gov.educ.api.studentassessment.model.dto.StudentAssessment;
 import ca.bc.gov.educ.api.studentassessment.model.transformer.StudentAssessmentTransformer;
 import ca.bc.gov.educ.api.studentassessment.repository.StudentAssessmentRepository;
 import ca.bc.gov.educ.api.studentassessment.util.StudentAssessmentApiConstants;
+import ca.bc.gov.educ.api.studentassessment.util.StudentAssessmentApiUtils;
 
 @Service
 public class StudentAssessmentService {
@@ -36,17 +40,19 @@ public class StudentAssessmentService {
 
      /**
      * Get all student assessments by PEN populated in Student Assessment DTO
+     * @param accessToken 
      *
      * @return Student Assessment 
      * @throws java.lang.Exception
      */
-    public List<StudentAssessment> getStudentAssessmentList(String pen) {
+    public List<StudentAssessment> getStudentAssessmentList(String pen, String accessToken) {
         List<StudentAssessment> studentAssessment  = new ArrayList<StudentAssessment>();
-
+        HttpHeaders httpHeaders = StudentAssessmentApiUtils.getHeaders(accessToken);
         try {
         	studentAssessment = studentAssessmentTransformer.transformToDTO(studentAssessmentRepo.findByPen(pen));
         	studentAssessment.forEach(sA -> {
-        		Assessment assessment = restTemplate.getForObject(String.format(getAssessmentByAssmtCodeURL, sA.getAssessmentCode()), Assessment.class);
+        		Assessment assessment = restTemplate.exchange(String.format(getAssessmentByAssmtCodeURL, sA.getAssessmentCode()), HttpMethod.GET,
+        				new HttpEntity<>(httpHeaders), Assessment.class).getBody();
         		if(assessment != null) {
         			sA.setAssessmentName(assessment.getAssessmentName());
         		}
