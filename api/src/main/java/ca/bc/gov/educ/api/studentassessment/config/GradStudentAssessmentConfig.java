9503eda46f21a787cc26aff1a236025b6a411d30
s.karekkattumanasree <s.karekkattumanasree@CA-L9Z2KNN2.groupinfra.com>
2021-03-03 12:31:25 -0800

    Student Assessment Changes

---
+++ api/src/main/java/ca/bc/gov/educ/api/studentassessment/config/GradStudentAssessmentConfig.java
@@ -0,0 +1,30 @@
+package ca.bc.gov.educ.api.studentassessment.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import ca.bc.gov.educ.api.studentassessment.util.MessageHelper;
+
+
+@Configuration
+@PropertySource("classpath:messages.properties")
+public class GradStudentAssessmentConfig implements WebMvcConfigurer {
+
+	@Autowired
+	RequestInterceptor requestInterceptor;
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(requestInterceptor);
+	}
+
+	@Bean
+	public MessageHelper messageHelper() {
+		return new MessageHelper();
+	}
+
+	
+}
