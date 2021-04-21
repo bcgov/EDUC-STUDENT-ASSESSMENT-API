16d24b34fc4ece7af975613cf8cc47044ea2214d
s.karekkattumanasree <s.karekkattumanasree@CA-L9Z2KNN2.groupinfra.com>
2021-04-20 18:04:51 -0700

    Changing to webclient

--- api/src/main/java/ca/bc/gov/educ/api/studentassessment/EducStudentAssessmentApiApplication.java
+++ api/src/main/java/ca/bc/gov/educ/api/studentassessment/EducStudentAssessmentApiApplication.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import ca.bc.gov.educ.api.studentassessment.model.dto.StudentAssessment;
 import ca.bc.gov.educ.api.studentassessment.model.entity.StudentAssessmentEntity;
@@ -39,6 +40,11 @@ public class EducStudentAssessmentApiApplication {
 		modelMapper.typeMap(StudentAssessmentEntity.class, StudentAssessment.class);
 		modelMapper.typeMap(StudentAssessment.class, StudentAssessmentEntity.class);
 		return modelMapper;
+	}
+	
+	@Bean
+	public WebClient webClient() {
+		return WebClient.create();
 	}
 	
 	@Bean
