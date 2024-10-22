f2670e3d1a2680d224caf92a764655c4a0730811
QSL\SumathiT <sumathi.thirumani@quartech.com>
2024-10-21 17:14:13 -0700

    1. Get students using assessment ID 2. Get students using specific session ID and assessmenttype 3. Get students using school year 4. Get students using school year and special code

--- api/src/test/java/ca/bc/gov/educ/eas/api/controller/v1/AssessmentStudentControllerTest.java
+++ api/src/test/java/ca/bc/gov/educ/eas/api/controller/v1/AssessmentStudentControllerTest.java
@@ -2,6 +2,7 @@ package ca.bc.gov.educ.eas.api.controller.v1;
 
 import ca.bc.gov.educ.eas.api.BaseEasAPITest;
 import ca.bc.gov.educ.eas.api.constants.v1.AssessmentTypeCodes;
+import ca.bc.gov.educ.eas.api.constants.v1.ProvincialSpecialCaseCodes;
 import ca.bc.gov.educ.eas.api.constants.v1.URL;
 import ca.bc.gov.educ.eas.api.filter.FilterOperation;
 import ca.bc.gov.educ.eas.api.mappers.v1.AssessmentStudentMapper;
@@ -22,10 +23,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.time.LocalDateTime;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.UUID;
 
+import static ca.bc.gov.educ.eas.api.struct.v1.Condition.AND;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
@@ -283,7 +286,7 @@ class AssessmentStudentControllerTest extends BaseEasAPITest {
             .build();
 
     SearchCriteria criteriaSessionMonth = SearchCriteria.builder()
-            .condition(Condition.AND)
+            .condition(AND)
             .key("assessmentEntity.sessionEntity.courseMonth")
             .operation(FilterOperation.EQUAL)
             .value("08")
@@ -293,6 +296,150 @@ class AssessmentStudentControllerTest extends BaseEasAPITest {
     List<SearchCriteria> criteriaList = new LinkedList<>();
     criteriaList.add(criteriaAssessmentTypeCode);
     criteriaList.add(criteriaSessionMonth);
+    List<Search> searches = new LinkedList<>();
+    searches.add(Search.builder().searchCriteriaList(criteriaList).build());
+    ObjectMapper objectMapper = new ObjectMapper();
+    String criteriaJSON = objectMapper.writeValueAsString(searches);
+
+    final MvcResult result = this.mockMvc.perform(
+                    get(URL.BASE_URL_STUDENT + URL.PAGINATED)
+                            .with(mockAuthority)
+                            .param("searchCriteriaList", criteriaJSON)
+                            .contentType(APPLICATION_JSON))
+            .andDo(print())
+            .andReturn();
+    this.mockMvc.perform(asyncDispatch(result)).andDo(print()).andExpect(status().isOk()).andExpect(jsonPath("$.content", hasSize(1)));
+  }
+
+  @Test
+  void testFindAll_GivenAssessmentID_ShouldReturnStudent() throws Exception {
+    final GrantedAuthority grantedAuthority = () -> "SCOPE_READ_EAS_STUDENT";
+    final SecurityMockMvcRequestPostProcessors.OidcLoginRequestPostProcessor mockAuthority = oidcLogin().authorities(grantedAuthority);
+
+    SessionEntity session = createMockSessionEntity();
+    session.setCourseMonth("08");
+    SessionEntity sessionEntity = sessionRepository.save(session);
+    AssessmentEntity assessment = assessmentRepository.save(createMockAssessmentEntity(sessionEntity, AssessmentTypeCodes.LTP10.getCode()));
+
+    AssessmentStudentEntity student = createMockStudentEntity(assessment);
+    studentRepository.save(student);
+
+    AssessmentEntity assessment2 = assessmentRepository.save(createMockAssessmentEntity(sessionEntity, AssessmentTypeCodes.LTF12.getCode()));
+    AssessmentStudentEntity student2 = createMockStudentEntity(assessment2);
+    studentRepository.save(student2);
+
+    AssessmentStudentEntity student3 = createMockStudentEntity(assessment2);
+    studentRepository.save(student3);
+
+    SearchCriteria criteriaAssessmentID = SearchCriteria.builder()
+            .key("assessmentEntity.assessmentID")
+            .operation(FilterOperation.EQUAL)
+            .value(assessment2.getAssessmentID().toString())
+            .valueType(ValueType.UUID)
+            .build();
+
+
+    List<SearchCriteria> criteriaList = new LinkedList<>();
+    criteriaList.add(criteriaAssessmentID);
+    List<Search> searches = new LinkedList<>();
+    searches.add(Search.builder().searchCriteriaList(criteriaList).build());
+    ObjectMapper objectMapper = new ObjectMapper();
+    String criteriaJSON = objectMapper.writeValueAsString(searches);
+
+    final MvcResult result = this.mockMvc.perform(
+                    get(URL.BASE_URL_STUDENT + URL.PAGINATED)
+                            .with(mockAuthority)
+                            .param("searchCriteriaList", criteriaJSON)
+                            .contentType(APPLICATION_JSON))
+            .andDo(print())
+            .andReturn();
+    this.mockMvc.perform(asyncDispatch(result)).andDo(print()).andExpect(status().isOk()).andExpect(jsonPath("$.content", hasSize(2)));
+  }
+
+  @Test
+  void testFindAll_GivenSchoolYear_ShouldReturnStudent() throws Exception {
+    final GrantedAuthority grantedAuthority = () -> "SCOPE_READ_EAS_STUDENT";
+    final SecurityMockMvcRequestPostProcessors.OidcLoginRequestPostProcessor mockAuthority = oidcLogin().authorities(grantedAuthority);
+
+    SessionEntity session = createMockSessionEntity();
+    session.setCourseMonth("08");
+    SessionEntity sessionEntity = sessionRepository.save(session);
+    AssessmentEntity assessment = assessmentRepository.save(createMockAssessmentEntity(sessionEntity, AssessmentTypeCodes.LTP10.getCode()));
+
+    AssessmentStudentEntity student = createMockStudentEntity(assessment);
+    studentRepository.save(student);
+
+    AssessmentEntity assessment2 = assessmentRepository.save(createMockAssessmentEntity(sessionEntity, AssessmentTypeCodes.LTF12.getCode()));
+    AssessmentStudentEntity student2 = createMockStudentEntity(assessment2);
+    studentRepository.save(student2);
+
+    AssessmentStudentEntity student3 = createMockStudentEntity(assessment2);
+    studentRepository.save(student3);
+
+    SearchCriteria criteriaAssessmentID = SearchCriteria.builder()
+            .key("assessmentEntity.sessionEntity.schoolYear")
+            .operation(FilterOperation.EQUAL)
+            .value(String.valueOf(LocalDateTime.now().getYear()))
+            .valueType(ValueType.INTEGER)
+            .build();
+
+
+    List<SearchCriteria> criteriaList = new LinkedList<>();
+    criteriaList.add(criteriaAssessmentID);
+    List<Search> searches = new LinkedList<>();
+    searches.add(Search.builder().searchCriteriaList(criteriaList).build());
+    ObjectMapper objectMapper = new ObjectMapper();
+    String criteriaJSON = objectMapper.writeValueAsString(searches);
+
+    final MvcResult result = this.mockMvc.perform(
+                    get(URL.BASE_URL_STUDENT + URL.PAGINATED)
+                            .with(mockAuthority)
+                            .param("searchCriteriaList", criteriaJSON)
+                            .contentType(APPLICATION_JSON))
+            .andDo(print())
+            .andReturn();
+    this.mockMvc.perform(asyncDispatch(result)).andDo(print()).andExpect(status().isOk()).andExpect(jsonPath("$.content", hasSize(3)));
+  }
+
+  @Test
+  void testFindAll_GivenSchoolYearAndSpecialCode_ShouldReturnStudent() throws Exception {
+    final GrantedAuthority grantedAuthority = () -> "SCOPE_READ_EAS_STUDENT";
+    final SecurityMockMvcRequestPostProcessors.OidcLoginRequestPostProcessor mockAuthority = oidcLogin().authorities(grantedAuthority);
+
+    SessionEntity session = createMockSessionEntity();
+    session.setCourseMonth("08");
+    SessionEntity sessionEntity = sessionRepository.save(session);
+    AssessmentEntity assessment = assessmentRepository.save(createMockAssessmentEntity(sessionEntity, AssessmentTypeCodes.LTP10.getCode()));
+
+    AssessmentStudentEntity student1 = createMockStudentEntity(assessment);
+    studentRepository.save(student1);
+
+    AssessmentEntity assessment2 = assessmentRepository.save(createMockAssessmentEntity(sessionEntity, AssessmentTypeCodes.LTF12.getCode()));
+    AssessmentStudentEntity student2 = createMockStudentEntity(assessment2);
+    studentRepository.save(student2);
+
+    AssessmentStudentEntity student3 = createMockStudentEntity(assessment2);
+    student3.setProvincialSpecialCaseCode(ProvincialSpecialCaseCodes.EXEMPTION.getCode());
+    studentRepository.save(student3);
+    SearchCriteria criteriaAssessmentID = SearchCriteria.builder()
+            .key("assessmentEntity.sessionEntity.schoolYear")
+            .operation(FilterOperation.EQUAL)
+            .value(String.valueOf(LocalDateTime.now().getYear()))
+            .valueType(ValueType.INTEGER)
+            .build();
+
+    SearchCriteria criteriaSpecialCaseCode = SearchCriteria.builder()
+            .condition(AND)
+            .key("provincialSpecialCaseCode")
+            .operation(FilterOperation.IN)
+            .value("E,D")
+            .valueType(ValueType.STRING)
+            .build();
+
+
+    List<SearchCriteria> criteriaList = new LinkedList<>();
+    criteriaList.add(criteriaAssessmentID);
+    criteriaList.add(criteriaSpecialCaseCode);
     List<Search> searches = new LinkedList<>();
     searches.add(Search.builder().searchCriteriaList(criteriaList).build());
     ObjectMapper objectMapper = new ObjectMapper();
