72fe8c1b10ebc131c9765a392eb0adccf7c6ec76
QSL\SumathiT <sumathi.thirumani@quartech.com>
2024-10-25 13:53:35 -0700

    Remove explicit retry and leverage default saga mechanism retry & tracking.

--- api/src/main/java/ca/bc/gov/educ/eas/api/service/v1/StudentRegistrationOrchestrationService.java
+++ api/src/main/java/ca/bc/gov/educ/eas/api/service/v1/StudentRegistrationOrchestrationService.java
@@ -1,14 +1,11 @@
 package ca.bc.gov.educ.eas.api.service.v1;
 
-import ca.bc.gov.educ.eas.api.exception.SagaRuntimeException;
 import ca.bc.gov.educ.eas.api.model.v1.EasSagaEntity;
 import ca.bc.gov.educ.eas.api.service.v1.external.IDownstreamSyncService;
 import ca.bc.gov.educ.eas.api.struct.v1.AssessmentStudent;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.retry.annotation.Backoff;
-import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -29,7 +26,6 @@ public class StudentRegistrationOrchestrationService {
         this.sagaService = sagaService;
     }
 
-    @Retryable(maxAttempts = 5, retryFor = {Exception.class}, noRetryFor = {SagaRuntimeException.class}, backoff = @Backoff(multiplier = 2, delay = 2000))
     public void publishStudentRegistration(AssessmentStudent assessmentStudent, EasSagaEntity sagaEntity) {
         downstreamSyncService.publishRegistration(assessmentStudent);
     }
