8a87100685dd9ef36c818fafc38909cb5114eedd
Eckerman <erineckerman@gmail.com>
2024-12-05 10:22:16 -0800

    Remove unused structs

--- api/src/main/java/ca/bc/gov/educ/eas/api/rules/assessment/AssessmentStudentValidationIssueTypeCode.java
+++ api/src/main/java/ca/bc/gov/educ/eas/api/rules/assessment/AssessmentStudentValidationIssueTypeCode.java
@@ -7,12 +7,9 @@ import java.util.Map;
 
 public enum AssessmentStudentValidationIssueTypeCode {
 
-    DEM_DATA_MISSING("DEM_DATA_MISSING", "This student is missing demographic data based on Student PEN, Surname, and Given Name."),
-    COURSE_CODE_INVALID("COURSE_CODE_INVALID", "The Assessment Code provided is not valid for the Assessment Session specified."),
     COURSE_SESSION_DUP("COURSE_SESSION_DUP", "The assessment session is a duplicate of an existing assessment session for this student/assessment/level."),
     COURSE_SESSION_EXCEED("COURSE_SESSION_EXCEED", "Student has already reached the maximum number of writes for this Assessment."),
     COURSE_STATUS_INVALID("COURSE_STATUS_INVALID", "Assessment registration must be A=active or W=withdraw."),
-    NUMBER_OF_CREDITS_NOT_BLANK("NUMBER_OF_CREDITS_NOT_BLANK", "Number of credits value is ignored and must be blank."),
     EXAM_SCHOOL_INVALID("EXAM_SCHOOL_INVALID", "Invalid assessment center provided."),
     SCHOOL_INVALID("SCHOOL_INVALID", "Invalid school provided."),
     COURSE_CODE_CSF("COURSE_CODE_CSF", "Student is in a Francophone school and cannot register for this assessment session for this student."),
