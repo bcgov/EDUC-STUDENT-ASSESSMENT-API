80d3711f19def2f2640e8e6830db7d7405a0751b
arcshiftsolutions <arcshiftsolutions@gmail.com>
2025-07-03 17:38:44 -0700

    Updates for assessment type descriptions and labels

---
+++ api/src/main/resources/db/migration/V1.57__ASSESSMENT.API.sql
@@ -0,0 +1,14 @@
+UPDATE assessment_type_code
+SET label='Grade 10 Literacy Assessment', 
+    description='Grade 10 Literacy Assessment'
+WHERE assessment_type_code='LTE10';
+
+UPDATE assessment_type_code
+SET label='Grade 12 Literacy Assessment',
+    description='Grade 12 Literacy Assessment'
+WHERE assessment_type_code='LTE12';
+
+UPDATE assessment_type_code
+SET label='Grade 10 Numeracy Assessment',
+    description='Grade 10 Numeracy Assessment'
+WHERE assessment_type_code='NME10';
