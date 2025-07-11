b5c4f27652885cb783a9740e2dd5baf55c6aea77
arcshiftsolutions <arcshiftsolutions@gmail.com>
2025-07-11 16:09:03 -0700

    Small fix

--- api/src/main/java/ca/bc/gov/educ/assessment/api/reports/SchoolStudentsInSessionReportService.java
+++ api/src/main/java/ca/bc/gov/educ/assessment/api/reports/SchoolStudentsInSessionReportService.java
@@ -28,9 +28,7 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.UUID;
 
-/**
- * Service class to generate School Students in Session Report
- */
+
 @Service
 @Slf4j
 public class SchoolStudentsInSessionReportService extends BaseReportGenerationService{
@@ -60,9 +58,6 @@ public class SchoolStudentsInSessionReportService extends BaseReportGenerationSe
 
   private void compileJasperReports(){
     try {
-      // Set temp directory for JasperReports compilation
-      System.setProperty("jasper.reports.compile.temp", System.getProperty("java.io.tmpdir"));
-      
       InputStream inputHeadcount = getClass().getResourceAsStream("/reports/schoolStudentsInSession.jrxml");
       log.info("Compiling Jasper reports");
       schoolStudentInSessionReport = JasperCompileManager.compileReport(inputHeadcount);
