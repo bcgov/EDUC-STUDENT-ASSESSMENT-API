0e3d1ca1de0b9494825e98e053245c0d39605db0
keeganbcgov <Keegan.Laur@gov.bc.ca>
2025-07-16 15:54:01 -0700

    EAC-66: adding grade at registration to AssessmentStudent.

--- api/src/main/java/ca/bc/gov/educ/assessment/api/model/v1/AssessmentStudentEntity.java
+++ api/src/main/java/ca/bc/gov/educ/assessment/api/model/v1/AssessmentStudentEntity.java
@@ -62,6 +62,9 @@ public class AssessmentStudentEntity {
   @Column(name = "LOCAL_ID", length = 12)
   private String localID;
 
+  @Column(name = "GRADE_AT_REGISTRATION", length = 3)
+  private String gradeAtRegistration;
+
   @Column(name = "LOCAL_ASSESSMENT_ID", length = 20)
   private String localAssessmentID;
 
