0614933c0a35c633960b485bc89b222b1e322644
Avisha Sodhi <38086281+SodhiA1@users.noreply.github.com>
2025-07-28 13:18:35 -0700

    lint fixes

--- api/src/main/java/ca/bc/gov/educ/assessment/api/repository/v1/AssessmentStudentLightRepository.java
+++ api/src/main/java/ca/bc/gov/educ/assessment/api/repository/v1/AssessmentStudentLightRepository.java
@@ -2,15 +2,11 @@ package ca.bc.gov.educ.assessment.api.repository.v1;
 
 import ca.bc.gov.educ.assessment.api.model.v1.AssessmentStudentEntity;
 import ca.bc.gov.educ.assessment.api.model.v1.AssessmentStudentLightEntity;
-import ca.bc.gov.educ.assessment.api.model.v1.StagedAssessmentStudentEntity;
-import ca.bc.gov.educ.assessment.api.struct.v1.reports.RegistrationSummaryResult;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
