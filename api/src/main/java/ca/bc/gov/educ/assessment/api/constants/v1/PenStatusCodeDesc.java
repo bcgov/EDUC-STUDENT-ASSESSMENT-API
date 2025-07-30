b7314a06cc757ebc138827bc89dd89f18c56d6e1
Avisha Sodhi <38086281+SodhiA1@users.noreply.github.com>
2025-07-30 15:02:29 -0700

    Update PenStatusCodeDesc.java

--- api/src/main/java/ca/bc/gov/educ/assessment/api/constants/v1/PenStatusCodeDesc.java
+++ api/src/main/java/ca/bc/gov/educ/assessment/api/constants/v1/PenStatusCodeDesc.java
@@ -2,10 +2,6 @@ package ca.bc.gov.educ.assessment.api.constants.v1;
 
 import lombok.Getter;
 
-import java.util.Arrays;
-import java.util.Objects;
-import java.util.Optional;
-
 @Getter
 public enum PenStatusCodeDesc {
   MERGED("PEN MERGED"),
