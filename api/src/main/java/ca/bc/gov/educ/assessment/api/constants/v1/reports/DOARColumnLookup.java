662a1b694fd2f731f042e31da8413d4bf7f87eb4
Avisha Sodhi <38086281+SodhiA1@users.noreply.github.com>
2025-08-15 15:54:37 -0700

    Added missing reports

--- api/src/main/java/ca/bc/gov/educ/assessment/api/constants/v1/reports/DOARColumnLookup.java
+++ api/src/main/java/ca/bc/gov/educ/assessment/api/constants/v1/reports/DOARColumnLookup.java
@@ -4,28 +4,26 @@ import java.util.Arrays;
 
 public enum DOARColumnLookup {
 
-    ENTRY1("BOTH", "P"),
-    ENTRY2("BOTH", "R"),
-    ENTRY3("BOTH", "F"),
-    ENTRY4("BOTH", "M"),
-    ENTRY5("MUL_CHOICE", "I"),
-    ENTRY6("MUL_CHOICE", "P"),
-    ENTRY7("MUL_CHOICE", "S"),
-    ENTRY8("MUL_CHOICE", "N"),
-    ENTRY9("BOTH", "7"),
-    ENTRY10("BOTH", "7"),
-    ENTRY11("BOTH", "9")
+    ENTRY1("BOTH", new String[] {"P", "R", "F", "M"}, new String[] {"NME"}),//task code
+    ENTRY2("MUL_CHOICE", new String[] {"I","P", "S", "N", "C"}, new String[] {"NME", "LTF12", "LTP10", "LTP12", "LTE"}), // claim code
+    ENTRY3("BOTH", new String[] {"7", "8", "9"}, new String[] {"NME", "LTE", "LTP12", "LTF12", "LTP10"}), // cogn. code
+    ENTRY4("OPEN_ENDED", new String[] {"W", "O"}, new String[] {"LTF12", "LTP10", "LTP12", "LTE"}),// claim code
+    ENTRY5("MUL_CHOICE", new String[] {"A", "I", "E"}, new String[] {"LTF12"}), //task code
+    ENTRY6("OPEN_ENDED", new String[] {"WRS", "WRD", "WRF", "O1D", "O1F", "O1E", "O2D", "O2F", "O2E", "GO", "WRA", "WRB", "O1", "O2", "O3"}, new String[] {"LTF12", "LTP10", "LTP12", "LTE"}), //concepts code
+    ENTRY7("MUL_CHOICE", new String[] {"A", "B"}, new String[] {"LTE"}) //assmt. section
     ;
 
     private final String componentType;
-    private final String code;
-    DOARColumnLookup(String componentType, String code) {
+    private final String[] code;
+    private final String[] assessmentTypeCode;
+    DOARColumnLookup(String componentType, String[] code, String[] assessmentTypeCode) {
         this.componentType = componentType;
         this.code= code;
+        this.assessmentTypeCode = assessmentTypeCode;
     }
-    public static DOARColumnLookup getDOARColumn(String componentType, String code) {
+    public static DOARColumnLookup getDOARColumn(String componentType, String code, String assessmentTypeCode) {
         return Arrays.stream(values())
-                .filter(e -> e.code.equalsIgnoreCase(code) && e.componentType.equalsIgnoreCase(componentType))
+                .filter(e -> Arrays.asList(e.code).contains(code) && e.componentType.equalsIgnoreCase(componentType) && Arrays.asList(e.assessmentTypeCode).contains(assessmentTypeCode))
                 .findFirst().orElse(null);
     }
 
