6de73481043c657b9aec7b0317838af9143baf2f
alexmcdermid <alexander.mcdermid@alumni.ubc.ca>
2025-08-29 15:22:27 -0700

    MIN - View a Student's Assessment Details (raw scores)

---
+++ api/src/main/java/ca/bc/gov/educ/assessment/api/mappers/v1/AssessmentStudentShowItemMapper.java
@@ -0,0 +1,29 @@
+package ca.bc.gov.educ.assessment.api.mappers.v1;
+
+import ca.bc.gov.educ.assessment.api.mappers.LocalDateTimeMapper;
+import ca.bc.gov.educ.assessment.api.mappers.UUIDMapper;
+import ca.bc.gov.educ.assessment.api.model.v1.AssessmentStudentEntity;
+import ca.bc.gov.educ.assessment.api.struct.v1.AssessmentStudentShowItem;
+import org.mapstruct.DecoratedWith;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(uses = {UUIDMapper.class, LocalDateTimeMapper.class})
+@DecoratedWith(AssessmentStudentShowItemMapperDecorator.class)
+public interface AssessmentStudentShowItemMapper {
+    AssessmentStudentShowItemMapper mapper = Mappers.getMapper(AssessmentStudentShowItemMapper.class);
+
+    @Mapping(target = "assessmentID", source = "assessmentEntity.assessmentID")
+    @Mapping(target = "sessionID", source = "assessmentEntity.assessmentSessionEntity.sessionID")
+    @Mapping(target = "assessmentTypeCode", source = "assessmentEntity.assessmentTypeCode")
+    @Mapping(target = "courseMonth", source = "assessmentEntity.assessmentSessionEntity.courseMonth")
+    @Mapping(target = "courseYear", source = "assessmentEntity.assessmentSessionEntity.courseYear")
+    @Mapping(target = "assessmentCenterSchoolID", source = "assessmentCenterSchoolID")
+    @Mapping(target = "surname", source = "surname")
+    @Mapping(target = "givenName", source = "givenName")
+    @Mapping(target = "rawScore", source = "rawScore")
+    @Mapping(target = "mcTotal", source = "mcTotal")
+    @Mapping(target = "oeTotal", source = "oeTotal")
+    AssessmentStudentShowItem toStructure(AssessmentStudentEntity entity);
+}
