520d472b2aa16b379bfef78380fe529ab186049e
Avisha Sodhi <38086281+SodhiA1@users.noreply.github.com>
2025-09-12 11:14:00 -0700

    jetstream scheduler fix

--- api/src/main/java/ca/bc/gov/educ/assessment/api/repository/v1/AssessmentEventRepository.java
+++ api/src/main/java/ca/bc/gov/educ/assessment/api/repository/v1/AssessmentEventRepository.java
@@ -3,8 +3,10 @@ package ca.bc.gov.educ.assessment.api.repository.v1;
 
 import ca.bc.gov.educ.assessment.api.model.v1.AssessmentEventEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -13,9 +15,17 @@ import java.util.UUID;
 @Repository
 public interface AssessmentEventRepository extends JpaRepository<AssessmentEventEntity, UUID> {
 
-  Optional<AssessmentEventEntity> findBySagaIdAndEventType(UUID sagaId, String eventType);
+  List<AssessmentEventEntity> findByEventStatusAndEventTypeIn(String eventStatus, List<String> eventTypes);
 
   List<AssessmentEventEntity> findByEventStatus(String eventStatus);
 
   Optional<AssessmentEventEntity> findByEventId(UUID eventId);
+
+  @Query(value = "select event.* from ASSESSMENT_EVENT event where event.EVENT_STATUS = :eventStatus " +
+          "AND event.CREATE_DATE < :createDate " +
+          "AND event.EVENT_TYPE not in :eventTypes " +
+          "ORDER BY event.CREATE_DATE asc " +
+          "FETCH FIRST :limit ROWS ONLY", nativeQuery=true)
+  List<AssessmentEventEntity> findAllByEventStatusAndCreateDateBeforeAndEventTypeNotInOrderByCreateDate(String eventStatus, LocalDateTime createDate, int limit, List<String> eventTypes);
+
 }
