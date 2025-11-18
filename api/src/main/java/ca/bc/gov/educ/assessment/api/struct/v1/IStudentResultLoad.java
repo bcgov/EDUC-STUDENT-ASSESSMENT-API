1e53c91493b6f74de4679e20e0a1ed1ef8a2c253
alexmcdermid <alexander.mcdermid@alumni.ubc.ca>
2025-11-18 12:01:08 -0800

    smells

--- api/src/main/java/ca/bc/gov/educ/assessment/api/struct/v1/IStudentResultLoad.java
+++ api/src/main/java/ca/bc/gov/educ/assessment/api/struct/v1/IStudentResultLoad.java
@@ -3,4 +3,5 @@ package ca.bc.gov.educ.assessment.api.struct.v1;
 public interface IStudentResultLoad {
     String getAssessmentID();
     String getPen();
+    String getStagedStudentResultID();
 }
