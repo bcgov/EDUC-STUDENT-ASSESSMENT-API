5cc0dda07e2a34417335738270df3961c6fa27e0
alexmcdermid <alexander.mcdermid@alumni.ubc.ca>
2025-11-19 16:00:31 -0800

    findTopLoadedStudentForProcessing

--- api/src/main/java/ca/bc/gov/educ/assessment/api/repository/v1/StagedStudentResultRepository.java
+++ api/src/main/java/ca/bc/gov/educ/assessment/api/repository/v1/StagedStudentResultRepository.java
@@ -17,11 +17,12 @@ import java.util.UUID;
 public interface StagedStudentResultRepository extends JpaRepository<StagedStudentResultEntity, UUID>, JpaSpecificationExecutor<StagedStudentResultEntity> {
 
     @Query(value="""
-    SELECT stud.assessmentEntity.assessmentID as assessmentID, stud.pen as pen, stud.stagedStudentResultID as stagedStudentResultID
-    FROM StagedStudentResultEntity stud    WHERE stud.stagedStudentResultStatus = 'LOADED'
-    AND (SELECT COUNT(saga) FROM AssessmentSagaEntity saga WHERE saga.status != 'COMPLETED' AND saga.stagedStudentResultID = stud.stagedStudentResultID AND saga.assessmentID=stud.assessmentEntity.assessmentID) = 0
-    GROUP BY stud.assessmentEntity.assessmentID, stud.pen
-    ORDER BY stud.stagedStudentResultID
+    SELECT stud.assessmentEntity.assessmentID as assessmentID, stud.pen as pen
+    FROM StagedStudentResultEntity stud
+    WHERE stud.stagedStudentResultStatus = 'LOADED'
+    AND (SELECT COUNT(saga) FROM AssessmentSagaEntity saga WHERE saga.status != 'COMPLETED' AND saga.pen = stud.pen AND saga.assessmentID=stud.assessmentEntity.assessmentID) = 0
+    GROUP BY stud.pen, stud.assessmentEntity.assessmentID
+    ORDER BY stud.pen
     LIMIT :numberOfStudentsToProcess""")
     List<IStudentResultLoad> findTopLoadedStudentForProcessing(String numberOfStudentsToProcess);
 
