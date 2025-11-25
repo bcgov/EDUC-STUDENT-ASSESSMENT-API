733401334f681743993dfdfd79c8c26c3c171e35
Avisha Sodhi <38086281+SodhiA1@users.noreply.github.com>
2025-11-25 11:37:33 -0800

    EGC-723

---
+++ api/src/main/java/ca/bc/gov/educ/assessment/api/model/v1/StagedAssessmentStudentDOARCalculationEntity.java
@@ -0,0 +1,158 @@
+package ca.bc.gov.educ.assessment.api.model.v1;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.PastOrPresent;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@DynamicUpdate
+@Entity
+@Builder
+@Table(name = "STAGED_ASSESSMENT_STUDENT_DOAR_CALCULATION")
+public class StagedAssessmentStudentDOARCalculationEntity {
+
+    @Id
+    @UuidGenerator
+    @Column(name = "ASSESSMENT_STUDENT_DOAR_CALC_ID", unique = true, updatable = false, columnDefinition = "BINARY(16)")
+    private UUID assessmentStudentDOARCalculationID;
+
+    @Column(name = "ASSESSMENT_STUDENT_ID")
+    private UUID assessmentStudentID;
+
+    @Column(name = "ASSESSMENT_ID")
+    private UUID assessmentID;
+// lte10, 12
+    @Column(name = "TASK_COMPREHEND")
+    private BigDecimal taskComprehend;
+
+    @Column(name = "TASK_COMMUNICATE")
+    private BigDecimal taskCommunicate;
+
+    @Column(name = "COMPREHEND_PART_A")
+    private BigDecimal comprehendPartA;
+
+    @Column(name = "COMPREHEND_PART_B")
+    private BigDecimal comprehendPartB;
+
+    @Column(name = "COMMUNICATE_GRAPHIC_ORG")
+    private BigDecimal communicateGraphicOrg;
+
+    @Column(name = "COMMUNICATE_UNDERSTANDING")
+    private BigDecimal communicateUnderstanding;
+
+    @Column(name = "COMMUNICATE_PERSONAL_CONN")
+    private BigDecimal communicatePersonalConn;
+
+    @Column(name = "DOK1")
+    private BigDecimal dok1;
+
+    @Column(name = "DOK2")
+    private BigDecimal dok2;
+
+    @Column(name = "DOK3")
+    private BigDecimal dok3;
+
+    //numeracy
+    @Column(name = "TASK_PLAN")
+    private BigDecimal taskPlan;
+
+    @Column(name = "TASK_ESTIMATE")
+    private BigDecimal taskEstimate;
+
+    @Column(name = "TASK_FAIR")
+    private BigDecimal taskFair;
+
+    @Column(name = "TASK_MODEL")
+    private BigDecimal taskModel;
+
+    @Column(name = "NUMERACY_INTERPRET")
+    private BigDecimal numeracyInterpret;
+
+    @Column(name = "NUMERACY_APPLY")
+    private BigDecimal numeracyApply;
+
+    @Column(name = "NUMERACY_SOLVE")
+    private BigDecimal numeracySolve;
+
+    @Column(name = "NUMERACY_ANALYZE")
+    private BigDecimal numeracyAnalyze;
+
+    //LTP12
+    @Column(name = "TASK_ORAL")
+    private BigDecimal taskOral;
+
+    @Column(name = "COMMUNICATE_ORAL_PART_1")
+    private BigDecimal communicateOralPart1;
+
+    @Column(name = "COMMUNICATE_ORAL_PART_2")
+    private BigDecimal communicateOralPart2;
+
+    @Column(name = "COMMUNICATE_ORAL_PART_3")
+    private BigDecimal communicateOralPart3;
+
+    //LTP10
+    @Column(name = "COMPREHEND_PART_A_SHORT")
+    private BigDecimal comprehendPartAShort;
+
+    //LTF12
+    @Column(name = "COMPREHEND_PART_A_TASK")
+    private BigDecimal comprehendPartATask;
+
+    @Column(name = "COMPREHEND_PART_B_INFO")
+    private BigDecimal comprehendPartBInfo;
+
+    @Column(name = "COMPREHEND_PART_B_EXP")
+    private BigDecimal comprehendPartBExp;
+
+    @Column(name = "DISSERTATION_BACKGROUND")
+    private BigDecimal dissertationBackground;
+
+    @Column(name = "DISSERTATION_FORM")
+    private BigDecimal dissertationForm;
+
+    @Column(name = "COMMUNICATE_ORAL_PART_1_BACKGROUND")
+    private BigDecimal communicateOralPart1Background;
+
+    @Column(name = "COMMUNICATE_ORAL_PART_1_FORM")
+    private BigDecimal communicateOralPart1Form;
+
+    @Column(name = "COMMUNICATE_ORAL_PART_1_EXPR")
+    private BigDecimal communicateOralPart1Expression;
+
+    @Column(name = "COMMUNICATE_ORAL_PART_2_BACKGROUND")
+    private BigDecimal communicateOralPart2Background;
+
+    @Column(name = "COMMUNICATE_ORAL_PART_2_FORM")
+    private BigDecimal communicateOralPart2Form;
+
+    @Column(name = "COMMUNICATE_ORAL_PART_2_EXPR")
+    private BigDecimal communicateOralPart2Expression;
+
+    @Column(name = "CREATE_USER", updatable = false, length = 100)
+    private String createUser;
+
+    @PastOrPresent
+    @Column(name = "CREATE_DATE", updatable = false)
+    private LocalDateTime createDate;
+
+    @Column(name = "UPDATE_USER", nullable = false, length = 100)
+    private String updateUser;
+
+    @PastOrPresent
+    @Column(name = "UPDATE_DATE", nullable = false)
+    private LocalDateTime updateDate;
+}
---
+++ api/src/main/java/ca/bc/gov/educ/assessment/api/service/v1/DOARStagingCalculateService.java
@@ -0,0 +1,219 @@
+package ca.bc.gov.educ.assessment.api.service.v1;
+
+
+import ca.bc.gov.educ.assessment.api.model.v1.*;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
+@Service
+@Slf4j
+@NoArgsConstructor
+public class DOARStagingCalculateService {
+
+    public BigDecimal calculateTotal(StagedAssessmentStudentEntity student, List<AssessmentQuestionEntity> selectedOeAssessmentQuestionsByTypeCode, List<AssessmentQuestionEntity> selectedMcAssessmentQuestionsByTypeCode) {
+        //possible total
+        BigDecimal possibleMcTotal = getPossibleMCTotal(selectedMcAssessmentQuestionsByTypeCode);
+
+        BigDecimal possibleOeTotal = getPossibleOETotal(selectedOeAssessmentQuestionsByTypeCode, student); // check per question
+
+        //student Total
+        BigDecimal studentMcTotal = getStudentMCTotal(selectedMcAssessmentQuestionsByTypeCode, student);
+        BigDecimal studentOeTotal = getStudentOETotal(selectedOeAssessmentQuestionsByTypeCode, student);
+
+        var studentTotal = studentMcTotal.add(studentOeTotal);
+        var possibleTotal = possibleMcTotal.add(possibleOeTotal);
+
+        if(possibleTotal.compareTo(BigDecimal.ZERO) != 0) {
+            return studentTotal.divide(possibleTotal, 4, RoundingMode.DOWN).multiply(BigDecimal.valueOf(100));
+        }
+
+        return BigDecimal.ZERO;
+    }
+
+    private boolean checkIfStudentAnsweredOEQues(StagedAssessmentStudentEntity student, List<AssessmentQuestionEntity> questions) {
+
+        //question ids by typecode
+        var listOfQuesIDs = questions.stream().map(AssessmentQuestionEntity::getAssessmentQuestionID).toList();
+
+        //assessment choice Ids of questions by typecode
+        var listOfQuesChoiceIDs =  questions.stream().map(AssessmentQuestionEntity::getAssessmentChoiceEntity)
+                .filter(Objects::nonNull).map(AssessmentChoiceEntity::getAssessmentChoiceID).toList();
+
+        //questionIds of questions student chose(includes answered and un-answered)
+        var listOfQuesAnswered = student.getStagedAssessmentStudentComponentEntities().stream()
+                .map(StagedAssessmentStudentComponentEntity::getStagedAssessmentStudentChoiceEntities).flatMap(Collection::stream)
+                .map(StagedAssessmentStudentChoiceEntity::getStagedAssessmentStudentChoiceQuestionSetEntities).flatMap(Collection::stream)
+                .map(StagedAssessmentStudentChoiceQuestionSetEntity::getAssessmentQuestionID).toList();
+
+        //assessment choice Ids of student answers
+        var listOfStudentChoices = student.getStagedAssessmentStudentComponentEntities().stream()
+                .map(StagedAssessmentStudentComponentEntity::getStagedAssessmentStudentChoiceEntities).flatMap(Collection::stream)
+                .map(StagedAssessmentStudentChoiceEntity::getAssessmentChoiceEntity).filter(Objects::nonNull).map(AssessmentChoiceEntity::getAssessmentChoiceID).toList();
+
+        var hasAnsweredOeQues =  listOfQuesAnswered.stream().anyMatch(listOfQuesIDs::contains);
+        var hasAnsweredADifferentChoice =  listOfStudentChoices.stream().anyMatch(listOfQuesChoiceIDs::contains);
+        if(hasAnsweredOeQues) {
+            return true;
+        } else return !hasAnsweredADifferentChoice;
+    }
+
+    public BigDecimal calculateMCTotal(List<AssessmentQuestionEntity> selectedMcAssessmentQuestionsByTypeCode, StagedAssessmentStudentEntity student) {
+        BigDecimal possibleMcTotal = getPossibleMCTotal(selectedMcAssessmentQuestionsByTypeCode);
+        BigDecimal studentMcTotal = getStudentMCTotal(selectedMcAssessmentQuestionsByTypeCode, student);
+
+        if(possibleMcTotal.compareTo(BigDecimal.ZERO) != 0) {
+            return  studentMcTotal.divide(possibleMcTotal, 4, RoundingMode.DOWN).multiply(BigDecimal.valueOf(100));
+        }
+
+        return BigDecimal.ZERO;
+    }
+
+    public BigDecimal calculateOETotal(List<AssessmentQuestionEntity> selectedMcAssessmentQuestionsByTypeCode, StagedAssessmentStudentEntity student) {
+        BigDecimal possibleOeTotal = getPossibleOETotal(selectedMcAssessmentQuestionsByTypeCode, student);
+        BigDecimal studentOeTotal = getStudentOETotal(selectedMcAssessmentQuestionsByTypeCode, student);
+
+        if(possibleOeTotal.compareTo(BigDecimal.ZERO) != 0) {
+            return studentOeTotal.divide(possibleOeTotal, 4, RoundingMode.DOWN).multiply(BigDecimal.valueOf(100));
+        }
+
+        return BigDecimal.ZERO;
+    }
+
+    private BigDecimal getPossibleMCTotal(List<AssessmentQuestionEntity> selectedMcAssessmentQuestionsByTypeCode) {
+        BigDecimal divisor = new BigDecimal(100);
+        if(selectedMcAssessmentQuestionsByTypeCode.isEmpty()) {
+            return BigDecimal.ZERO;
+        }
+        var totalQuestionValue = selectedMcAssessmentQuestionsByTypeCode.stream()
+                .map(question -> question.getQuestionValue().multiply(BigDecimal.valueOf(question.getScaleFactor())))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+        return totalQuestionValue.divide(divisor, 4, RoundingMode.DOWN);
+    }
+
+    private BigDecimal getPossibleOETotal(List<AssessmentQuestionEntity> selectedOeAssessmentQuestionsByTypeCode, StagedAssessmentStudentEntity student) {
+        if(selectedOeAssessmentQuestionsByTypeCode.isEmpty()) {
+            return BigDecimal.ZERO;
+        }
+        var oralQuestions = selectedOeAssessmentQuestionsByTypeCode.stream()
+                .filter(questionEntity -> questionEntity.getAssessmentComponentEntity().getComponentSubTypeCode().equalsIgnoreCase("ORAL"))
+                .toList();
+        var oralWrittenQuestions = selectedOeAssessmentQuestionsByTypeCode.stream()
+                .filter(questionEntity -> questionEntity.getAssessmentComponentEntity().getComponentSubTypeCode().equalsIgnoreCase("NONE"))
+                .toList();
+        Map<Integer, List<AssessmentQuestionEntity>> groupedOralQuestionsByMasterQuestionNumber = oralQuestions.stream().collect(Collectors.groupingBy(AssessmentQuestionEntity::getMasterQuestionNumber));
+        Map<Integer, List<AssessmentQuestionEntity>> groupedOralWrittenQuestionsByMasterQuestionNumber = oralWrittenQuestions.stream().collect(Collectors.groupingBy(AssessmentQuestionEntity::getMasterQuestionNumber));
+
+        BigDecimal possibleScore = BigDecimal.ZERO;
+        BigDecimal divisor = new BigDecimal(100);
+        for(List<AssessmentQuestionEntity> questionEntities : Stream.concat(groupedOralQuestionsByMasterQuestionNumber.values().stream(), groupedOralWrittenQuestionsByMasterQuestionNumber.values().stream()).toList()) {
+            var totalQuestionValue = questionEntities.getFirst().getQuestionValue();
+            var totalScale = questionEntities.getFirst().getScaleFactor();
+
+            boolean includeOeTotalInCalc = checkIfStudentAnsweredOEQues(student, questionEntities);
+            if(includeOeTotalInCalc) {
+                possibleScore = possibleScore.add(totalQuestionValue.multiply(BigDecimal.valueOf(totalScale)).divide(divisor, 4, RoundingMode.DOWN));
+            }
+        }
+        return possibleScore;
+    }
+
+    private BigDecimal getStudentMCTotal(List<AssessmentQuestionEntity> selectedMcAssessmentQuestionsByTypeCode, StagedAssessmentStudentEntity student) {
+        if(selectedMcAssessmentQuestionsByTypeCode.isEmpty()) {
+            return BigDecimal.ZERO;
+        }
+
+        var assessmentComponentID = selectedMcAssessmentQuestionsByTypeCode.getFirst().getAssessmentComponentEntity().getAssessmentComponentID();
+        // get student MC answers
+        var studentMcAnswers = student.getStagedAssessmentStudentComponentEntities().stream()
+                .filter(assessmentStudentComponentEntity -> Objects.equals(assessmentStudentComponentEntity.getAssessmentComponentID(), assessmentComponentID))
+                .map(StagedAssessmentStudentComponentEntity::getStagedAssessmentStudentAnswerEntities)
+                .flatMap(Collection::stream)
+                .toList();
+
+        BigDecimal studentScore = BigDecimal.ZERO;
+        for(AssessmentQuestionEntity  questionEntities : selectedMcAssessmentQuestionsByTypeCode) {
+            BigDecimal scaledScore =  calculateSingleMarkerScore(questionEntities, studentMcAnswers);
+            studentScore = studentScore.add(scaledScore);
+        }
+        return studentScore;
+    }
+
+    private BigDecimal getStudentOETotal(List<AssessmentQuestionEntity> selectedOeAssessmentQuestionsByTypeCode, StagedAssessmentStudentEntity student) {
+        if(selectedOeAssessmentQuestionsByTypeCode.isEmpty()) {
+            return BigDecimal.ZERO;
+        }
+
+        var oralQuestions = selectedOeAssessmentQuestionsByTypeCode.stream()
+                .filter(questionEntity -> questionEntity.getAssessmentComponentEntity().getComponentSubTypeCode().equalsIgnoreCase("ORAL"))
+                .toList();
+        var oralWrittenQuestions = selectedOeAssessmentQuestionsByTypeCode.stream()
+                .filter(questionEntity -> questionEntity.getAssessmentComponentEntity().getComponentSubTypeCode().equalsIgnoreCase("NONE"))
+                .toList();
+
+        var oralAssessmentComponentID = !oralQuestions.isEmpty() ? oralQuestions.getFirst().getAssessmentComponentEntity().getAssessmentComponentID() : null;
+        var oralWrittenAssessmentComponentID = !oralWrittenQuestions.isEmpty() ? oralWrittenQuestions.getFirst().getAssessmentComponentEntity().getAssessmentComponentID(): null;
+        // get student OE answers
+        var studentMcAnswers = student.getStagedAssessmentStudentComponentEntities().stream()
+                .filter(assessmentStudentComponentEntity ->
+                        (oralAssessmentComponentID != null && Objects.equals(assessmentStudentComponentEntity.getAssessmentComponentID(), oralAssessmentComponentID))
+                                || (oralWrittenAssessmentComponentID !=null && Objects.equals(assessmentStudentComponentEntity.getAssessmentComponentID(), oralWrittenAssessmentComponentID)))
+                .map(StagedAssessmentStudentComponentEntity::getStagedAssessmentStudentAnswerEntities)
+                .flatMap(Collection::stream)
+                .toList();
+
+        Map<Integer, List<AssessmentQuestionEntity>> groupedOralQuestionsByQuestionNumber = oralQuestions.stream().collect(Collectors.groupingBy(AssessmentQuestionEntity::getQuestionNumber));
+        Map<Integer, List<AssessmentQuestionEntity>> groupedOralWrittenQuestionsByQuestionNumber = oralWrittenQuestions.stream().collect(Collectors.groupingBy(AssessmentQuestionEntity::getQuestionNumber));
+
+        BigDecimal divisor = new BigDecimal(100);
+        BigDecimal calcScore = BigDecimal.ZERO;
+
+        for(List<AssessmentQuestionEntity> questionEntities : Stream.concat(groupedOralQuestionsByQuestionNumber.values().stream(), groupedOralWrittenQuestionsByQuestionNumber.values().stream()).toList()) {
+            if(questionEntities.size() > 1) {
+                var scaleFactor = questionEntities.getFirst().getScaleFactor();
+                var studentAnswers = studentMcAnswers.stream()
+                        .filter(assessmentStudentAnswerEntity -> questionEntities.stream()
+                                .anyMatch(assessmentQuestionEntity -> Objects.equals(assessmentStudentAnswerEntity.getAssessmentQuestionID(), assessmentQuestionEntity.getAssessmentQuestionID())))
+                        .toList();
+
+                if (!studentAnswers.isEmpty()) {
+                    var studentScore = studentAnswers.stream().map(StagedAssessmentStudentAnswerEntity::getScore)
+                            .map(bigDecimal -> bigDecimal.compareTo(new BigDecimal(9999)) == 0 ? BigDecimal.ZERO : bigDecimal)
+                            .reduce(BigDecimal.ZERO, BigDecimal::add).divide(BigDecimal.valueOf(studentAnswers.size()), 4, RoundingMode.DOWN);
+                    BigDecimal scaledScore = studentScore.multiply(BigDecimal.valueOf(scaleFactor));
+                    calcScore = calcScore.add(scaledScore.divide(divisor, 4, RoundingMode.DOWN));
+                }
+            } else {
+                BigDecimal scaledScore = calculateSingleMarkerScore(questionEntities.getFirst(), studentMcAnswers);
+                calcScore = calcScore.add(scaledScore);
+            }
+
+        }
+        return calcScore;
+    }
+
+    private BigDecimal calculateSingleMarkerScore(AssessmentQuestionEntity questionEntity, List<StagedAssessmentStudentAnswerEntity> studentMcAnswers) {
+        BigDecimal divisor = new BigDecimal(100);
+        var scaleFactor = questionEntity.getScaleFactor();
+        var studentAnswer = studentMcAnswers.stream()
+                .filter(assessmentStudentAnswerEntity -> Objects.equals(assessmentStudentAnswerEntity.getAssessmentQuestionID(), questionEntity.getAssessmentQuestionID()))
+                .findFirst();
+
+        if (studentAnswer.isPresent()) {
+            var studentScore = studentAnswer.get().getScore();
+            BigDecimal scaledScore = studentScore.compareTo(new BigDecimal(9999)) == 0 ? BigDecimal.ZERO : studentScore.multiply(BigDecimal.valueOf(scaleFactor));
+            return scaledScore.divide(divisor, 4, RoundingMode.DOWN);
+        }
+        return BigDecimal.ZERO;
+    }
+}
