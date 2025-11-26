ac6b890e5f6f54acde12736c650b7bddbda07329
Avisha Sodhi <38086281+SodhiA1@users.noreply.github.com>
2025-11-25 16:26:59 -0800

    UT.

---
+++ api/src/test/java/ca/bc/gov/educ/assessment/api/service/v1/DOARStagingReportServiceTest.java
@@ -0,0 +1,225 @@
+package ca.bc.gov.educ.assessment.api.service.v1;
+
+import ca.bc.gov.educ.assessment.api.BaseAssessmentAPITest;
+import ca.bc.gov.educ.assessment.api.model.v1.AssessmentFormEntity;
+import ca.bc.gov.educ.assessment.api.model.v1.AssessmentQuestionEntity;
+import ca.bc.gov.educ.assessment.api.repository.v1.*;
+import ca.bc.gov.educ.assessment.api.rest.RestUtils;
+import ca.bc.gov.educ.assessment.api.struct.external.institute.v1.SchoolTombstone;
+import ca.bc.gov.educ.assessment.api.struct.v1.StudentResultSagaData;
+import ca.bc.gov.educ.assessment.api.struct.v1.TransferOnApprovalSagaData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.when;
+
+class DOARStagingReportServiceTest extends BaseAssessmentAPITest {
+
+    @Autowired
+    private DOARStagingReportService doarReportService;
+    @Autowired
+    private AssessmentSessionRepository assessmentSessionRepository;
+    @Autowired
+    private AssessmentFormRepository assessmentFormRepository;
+    @Autowired
+    private AssessmentRepository assessmentRepository;
+    @Autowired
+    private AssessmentQuestionRepository assessmentQuestionRepository;
+    @Autowired
+    StagedAssessmentStudentRepository stagedAssessmentStudentRepository;
+    @Autowired
+    private RestUtils restUtils;
+    @Autowired
+    private AssessmentComponentRepository assessmentComponentRepository;
+    @Autowired
+    AssessmentStudentRepository studentRepository;
+    @Autowired
+    private StagedAssessmentStudentDOARCalculationRepository assessmentStudentDOARCalculationRepository;
+
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        assessmentStudentDOARCalculationRepository.deleteAll();
+        studentRepository.deleteAll();
+        stagedAssessmentStudentRepository.deleteAll();
+        assessmentQuestionRepository.deleteAll();
+        assessmentFormRepository.deleteAll();
+        assessmentRepository.deleteAll();
+        assessmentSessionRepository.deleteAll();
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "NMF10",
+            "LTE10",
+            "LTP10",
+            "LTF12",
+            "LTE12",
+            "NME10"
+    })
+    void createAndPopulateDOARSummaryCalculations_NMF10(String typeCode) {
+        var school = this.createMockSchool();
+        when(this.restUtils.getSchoolBySchoolID(anyString())).thenReturn(Optional.of(school));
+
+        AssessmentFormEntity formEntity = setData(typeCode, school);
+        var comps = assessmentComponentRepository.findByAssessmentFormEntity_AssessmentFormID(formEntity.getAssessmentFormID());
+        var comp1UUID = comps.get(0).getAssessmentComponentID();
+        var comp2UUID = comps.get(1).getAssessmentComponentID();
+        List<AssessmentQuestionEntity> ques1 = assessmentQuestionRepository.findByAssessmentComponentEntity_AssessmentComponentID(comp1UUID);
+        List<AssessmentQuestionEntity> ques2 = assessmentQuestionRepository.findByAssessmentComponentEntity_AssessmentComponentID(comp2UUID);
+
+        List<AssessmentQuestionEntity> ques = new ArrayList<>();
+        ques.addAll(ques1);
+        ques.addAll(ques2);
+
+        List<AssessmentQuestionEntity> updatedQues = new ArrayList<>();
+
+        for (int i = 1; i < ques.size(); i++) {
+            if (i % 2 == 0) {
+                ques.get(i).setTaskCode("P");
+                ques.get(i).setClaimCode("I");
+                ques.get(i).setCognitiveLevelCode("8");
+            } else if (i % 7 == 0) {
+                ques.get(i).setTaskCode("F");
+                ques.get(i).setClaimCode("S");
+                ques.get(i).setCognitiveLevelCode("7");
+            } else {
+                ques.get(i).setTaskCode("M");
+                ques.get(i).setClaimCode("P");
+                ques.get(i).setCognitiveLevelCode("9");
+            }
+            updatedQues.add(ques.get(i));
+        }
+        assessmentQuestionRepository.saveAll(updatedQues);
+
+        var student = stagedAssessmentStudentRepository.findByAssessmentFormIDIn(List.of(formEntity.getAssessmentFormID()));
+
+        var sagaData = StudentResultSagaData
+                .builder()
+                .assessmentID(String.valueOf(student.get(0).getAssessmentEntity().getAssessmentID()))
+                .pen(student.get(0).getPen())
+                .build();
+        doarReportService.createAndPopulateDOARStagingSummaryCalculations(sagaData);
+
+        var studentCalc = assessmentStudentDOARCalculationRepository.findByAssessmentStudentID(student.get(0).getAssessmentStudentID());
+        assertThat(studentCalc).isPresent();
+    }
+
+    @Test
+    void testDOARTotalForNME10Columns() {
+        var school = this.createMockSchool();
+        when(this.restUtils.getSchoolBySchoolID(anyString())).thenReturn(Optional.of(school));
+
+        AssessmentFormEntity formEntity = setData("NME10", school);
+        var comps = assessmentComponentRepository.findByAssessmentFormEntity_AssessmentFormID(formEntity.getAssessmentFormID());
+        var comp1UUID = comps.get(0).getAssessmentComponentID();
+        var comp2UUID = comps.get(1).getAssessmentComponentID();
+        List<AssessmentQuestionEntity> ques1 = assessmentQuestionRepository.findByAssessmentComponentEntity_AssessmentComponentID(comp1UUID);
+        List<AssessmentQuestionEntity> ques2 = assessmentQuestionRepository.findByAssessmentComponentEntity_AssessmentComponentID(comp2UUID);
+
+        List<AssessmentQuestionEntity> ques = new ArrayList<>();
+        ques.addAll(ques1);
+        ques.addAll(ques2);
+
+        List<AssessmentQuestionEntity> updatedQues = new ArrayList<>();
+
+        for (int i = 1; i < ques.size(); i++) {
+            if (i % 2 == 0) {
+                ques.get(i).setTaskCode("P");
+                ques.get(i).setClaimCode("I");
+                ques.get(i).setCognitiveLevelCode("8");
+            } else if (i % 7 == 0) {
+                ques.get(i).setTaskCode("F");
+                ques.get(i).setClaimCode("S");
+                ques.get(i).setCognitiveLevelCode("7");
+            } else {
+                ques.get(i).setTaskCode("M");
+                ques.get(i).setClaimCode("P");
+                ques.get(i).setCognitiveLevelCode("9");
+            }
+            updatedQues.add(ques.get(i));
+        }
+        assessmentQuestionRepository.saveAll(updatedQues);
+        var student = stagedAssessmentStudentRepository.findByAssessmentFormIDIn(List.of(formEntity.getAssessmentFormID()));
+        var sagaData = StudentResultSagaData
+                .builder()
+                .assessmentID(String.valueOf(student.get(0).getAssessmentEntity().getAssessmentID()))
+                .pen(student.get(0).getPen())
+                .build();
+        doarReportService.createAndPopulateDOARStagingSummaryCalculations(sagaData);
+
+        var studentCalc = assessmentStudentDOARCalculationRepository.findByAssessmentStudentID(student.get(0).getAssessmentStudentID());
+        assertThat(studentCalc).isPresent();
+    }
+
+    private AssessmentFormEntity setData(String assessmentTypeCode, SchoolTombstone schoolTombstone) {
+        var session = createMockSessionEntity();
+        session.setCourseMonth("01");
+        session.setCourseYear("2025");
+        session.setSchoolYear("2024/2025");
+        var savedSession = assessmentSessionRepository.save(session);
+        var savedAssessmentEntity = assessmentRepository.save(createMockAssessmentEntity(savedSession, assessmentTypeCode));
+
+        var savedForm = assessmentFormRepository.save(createMockAssessmentFormEntity(savedAssessmentEntity, "A"));
+
+        var savedMultiComp = assessmentComponentRepository.save(createMockAssessmentComponentEntity(savedForm, "MUL_CHOICE", "NONE"));
+        for(int i = 1;i < 29;i++) {
+            assessmentQuestionRepository.save(createMockAssessmentQuestionEntity(savedMultiComp, i, i));
+        }
+
+        var savedOpenEndedComp = assessmentComponentRepository.save(createMockAssessmentComponentEntity(savedForm, "OPEN_ENDED", "NONE"));
+        var q1 = createMockAssessmentQuestionEntity(savedOpenEndedComp, 2, 2);
+        q1.setMasterQuestionNumber(2);
+        var oe1 = assessmentQuestionRepository.save(q1);
+
+        var q2 = createMockAssessmentQuestionEntity(savedOpenEndedComp, 2, 3);
+        q2.setMasterQuestionNumber(2);
+        assessmentQuestionRepository.save(q2);
+
+        var q3 = createMockAssessmentQuestionEntity(savedOpenEndedComp, 4, 5);
+        q3.setMasterQuestionNumber(4);
+        assessmentQuestionRepository.save(q3);
+
+        var q4 = createMockAssessmentQuestionEntity(savedOpenEndedComp, 4, 6);
+        q4.setMasterQuestionNumber(4);
+        var oe4 = assessmentQuestionRepository.save(q4);
+
+        var studentEntity1 = createMockStagedStudentEntity(savedAssessmentEntity);
+        studentEntity1.setSchoolAtWriteSchoolID(UUID.fromString(schoolTombstone.getSchoolId()));
+        var componentEntity1 = createMockStagedAssessmentStudentComponentEntity(studentEntity1, savedMultiComp.getAssessmentComponentID());
+        var componentEntity2 = createMockStagedAssessmentStudentComponentEntity(studentEntity1, savedOpenEndedComp.getAssessmentComponentID());
+
+        var multiQues = assessmentQuestionRepository.findByAssessmentComponentEntity_AssessmentComponentID(savedMultiComp.getAssessmentComponentID());
+        for(int i = 1;i < multiQues.size() ;i++) {
+            if(i % 2 == 0) {
+                componentEntity1.getStagedAssessmentStudentAnswerEntities().add(createMockStagedAssessmentStudentAnswerEntity(multiQues.get(i).getAssessmentQuestionID(), BigDecimal.ZERO, componentEntity1));
+            } else {
+                componentEntity1.getStagedAssessmentStudentAnswerEntities().add(createMockStagedAssessmentStudentAnswerEntity(multiQues.get(i).getAssessmentQuestionID(), BigDecimal.ONE, componentEntity1));
+
+            }
+        }
+
+        componentEntity2.getStagedAssessmentStudentAnswerEntities().add(createMockStagedAssessmentStudentAnswerEntity(oe1.getAssessmentQuestionID(), BigDecimal.ONE, componentEntity2));
+        componentEntity2.getStagedAssessmentStudentAnswerEntities().add(createMockStagedAssessmentStudentAnswerEntity(oe4.getAssessmentQuestionID(), new BigDecimal(9999), componentEntity2));
+
+        studentEntity1.getStagedAssessmentStudentComponentEntities().addAll(List.of(componentEntity1, componentEntity2));
+        studentEntity1.setAssessmentFormID(savedForm.getAssessmentFormID());
+        stagedAssessmentStudentRepository.save(studentEntity1);
+
+        return savedForm;
+    }
+
+}
