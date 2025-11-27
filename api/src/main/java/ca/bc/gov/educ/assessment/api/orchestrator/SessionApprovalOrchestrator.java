70cff48337c5c13bccf9104cfd9d4ae4e037179f
alexmcdermid <alexander.mcdermid@alumni.ubc.ca>
2025-11-27 14:41:32 -0800

    student assessment api instead of system on xam file generation saga

--- api/src/main/java/ca/bc/gov/educ/assessment/api/orchestrator/SessionApprovalOrchestrator.java
+++ api/src/main/java/ca/bc/gov/educ/assessment/api/orchestrator/SessionApprovalOrchestrator.java
@@ -23,6 +23,7 @@ import static ca.bc.gov.educ.assessment.api.constants.EventOutcome.*;
 import static ca.bc.gov.educ.assessment.api.constants.EventType.*;
 import static ca.bc.gov.educ.assessment.api.constants.SagaEnum.GENERATE_XAM_FILES;
 import static ca.bc.gov.educ.assessment.api.constants.TopicsEnum.XAM_FILE_GENERATION_TOPIC;
+import static ca.bc.gov.educ.assessment.api.properties.ApplicationProperties.STUDENT_ASSESSMENT_API;
 
 @Slf4j
 @Component
@@ -136,7 +137,7 @@ public class SessionApprovalOrchestrator extends BaseOrchestrator<ApprovalSagaDa
         String payload = JsonUtil.getJsonStringFromObject(approvalSaga);
         AssessmentSagaEntity saga = sagaService.createSagaRecordInDB(
                 this.getSagaName(),
-                "system",
+                STUDENT_ASSESSMENT_API,
                 payload,
                 sessionID,
                 null,
