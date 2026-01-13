c4829a19b491381fdfaffe168c309be99bf3a9de
Avisha Sodhi <38086281+SodhiA1@users.noreply.github.com>
2026-01-13 15:06:13 -0800

    EGC-837: DOAR calc. fix

--- api/src/main/java/ca/bc/gov/educ/assessment/api/constants/v1/reports/DOARColumnLookup.java
+++ api/src/main/java/ca/bc/gov/educ/assessment/api/constants/v1/reports/DOARColumnLookup.java
@@ -8,7 +8,7 @@ public enum DOARColumnLookup {
     ENTRY2("MUL_CHOICE", new String[] {"I","P", "S", "N", "C"}, new String[] {"NME", "LTF12", "LTP10", "LTP12", "LTE"}), // claim code
     ENTRY3("BOTH", new String[] {"7", "8", "9"}, new String[] {"NME", "LTE", "LTP12", "LTF12", "LTP10"}), // cogn. code
     ENTRY4("OPEN_ENDED", new String[] {"W", "O"}, new String[] {"LTF12", "LTP10", "LTP12", "LTE"}),// claim code
-    ENTRY5("MUL_CHOICE", new String[] {"A", "I", "E"}, new String[] {"LTF12"}), //task code
+    ENTRY5("MUL_TASK_CHOICE", new String[] {"A", "I", "E"}, new String[] {"LTF12"}), //task code
     ENTRY6("OPEN_ENDED", new String[] {"WRS", "WRD", "WRF", "O1D", "O1F", "O1E", "O2D", "O2F", "O2E", "GO", "WRA", "WRB", "O1", "O2", "O3"}, new String[] {"LTF12", "LTP10", "LTP12", "LTE"}), //concepts code
     ENTRY7("MUL_CHOICE", new String[] {"A", "B"}, new String[] {"LTE", "LTP12", "LTP10"}) //assmt. section
     ;
