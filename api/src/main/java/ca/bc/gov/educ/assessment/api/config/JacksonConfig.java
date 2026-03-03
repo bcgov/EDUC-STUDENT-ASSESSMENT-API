2f4d701af6a1920538c9b0846f42371d964005ea
arcshiftsolutions <arcshiftsolutions@gmail.com>
2026-03-02 17:23:06 -0800

    Added a Jackson Config

---
+++ api/src/main/java/ca/bc/gov/educ/assessment/api/config/JacksonConfig.java
@@ -0,0 +1,21 @@
+package ca.bc.gov.educ.assessment.api.config;
+
+import com.fasterxml.jackson.core.StreamReadConstraints;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jacksonCustomizer() {
+        return builder -> builder.postConfigurer(mapper ->
+            mapper.getFactory().setStreamReadConstraints(
+                StreamReadConstraints.builder()
+                    .maxStringLength(50000000)
+                    .build()
+            )
+        );
+    }
+}
