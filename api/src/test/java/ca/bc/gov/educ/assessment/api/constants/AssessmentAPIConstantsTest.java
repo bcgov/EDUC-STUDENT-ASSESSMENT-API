540e78897c3b7e44a19ed40fb390b88208d197ee
alexmcdermid <alexander.mcdermid@alumni.ubc.ca>
2026-05-19 14:25:32 -0700

    update assessment_type_code order

--- api/src/test/java/ca/bc/gov/educ/assessment/api/constants/AssessmentAPIConstantsTest.java
+++ api/src/test/java/ca/bc/gov/educ/assessment/api/constants/AssessmentAPIConstantsTest.java
@@ -5,6 +5,8 @@ import ca.bc.gov.educ.assessment.api.constants.v1.AssessmentTypeCodes;
 import ca.bc.gov.educ.assessment.api.constants.v1.CourseStatusCodes;
 import org.junit.jupiter.api.Test;
 
+import java.util.Comparator;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,10 +21,25 @@ class AssessmentAPIConstantsTest extends BaseAssessmentAPITest {
 
     @Test
     void test_AssessmentTypeCodesSortOrderFor_KnownCodes_UsesEnumOrder() {
-        assertThat(AssessmentTypeCodes.sortOrderFor(AssessmentTypeCodes.LTE10.getCode()))
-                .isLessThan(AssessmentTypeCodes.sortOrderFor(AssessmentTypeCodes.LTP12.getCode()));
-        assertThat(AssessmentTypeCodes.sortOrderFor(AssessmentTypeCodes.NME10.getCode()))
+        List<String> reportOrder = List.of(
+                AssessmentTypeCodes.LTE10.getCode(),
+                AssessmentTypeCodes.LTP10.getCode(),
+                AssessmentTypeCodes.NME10.getCode(),
+                AssessmentTypeCodes.NMF10.getCode(),
+                AssessmentTypeCodes.LTE12.getCode(),
+                AssessmentTypeCodes.LTP12.getCode(),
+                AssessmentTypeCodes.LTF12.getCode()
+        );
+
+        assertThat(reportOrder.stream()
+                .sorted(Comparator.comparingInt(AssessmentTypeCodes::sortOrderFor))
+                .toList()).containsExactlyElementsOf(reportOrder);
+        assertThat(AssessmentTypeCodes.sortOrderFor(AssessmentTypeCodes.NME.getCode()))
+                .isGreaterThan(AssessmentTypeCodes.sortOrderFor(AssessmentTypeCodes.NME10.getCode()))
                 .isLessThan(AssessmentTypeCodes.sortOrderFor(AssessmentTypeCodes.NMF10.getCode()));
+        assertThat(AssessmentTypeCodes.sortOrderFor(AssessmentTypeCodes.NMF.getCode()))
+                .isGreaterThan(AssessmentTypeCodes.sortOrderFor(AssessmentTypeCodes.NMF10.getCode()))
+                .isLessThan(AssessmentTypeCodes.sortOrderFor(AssessmentTypeCodes.LTE12.getCode()));
     }
 
     @Test
